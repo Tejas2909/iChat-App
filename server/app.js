3c655ef17a8227d3447a7fbe1946089ca9514893
Tejas Jhamnani <tejasj2909@gmail.com>
2021-05-25 16:20:56 +0530

    made responsive changes

--- server/app.js
+++ server/app.js
@@ -6,11 +6,20 @@ const router = require("./routers/routes");
 const sockets = require("./controllers/sockets");
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 dbConnect();
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.use("/api", router);
 const server = app.listen(PORT, () => {
   console.log(`server running at port ${PORT}`);
