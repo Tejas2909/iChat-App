478c537e8937a60ed28d06275786ae839693553a
Tejas Jhamnani <tejasj2909@gmail.com>
2021-08-27 17:36:04 +0530

    updated iChat-App

--- server/controllers/CreateRoom.js
+++
@@ -1,6 +0,0 @@
-const Room = require("../models/Rooms");
-const CreateRoom = (req, res) => {
-  console.log(req.body);
-  res.json({ status: 200, msg: "room created" });
-};
-module.exports = CreateRoom;
--- server/controllers/JoinRoom.js
+++
@@ -1,6 +0,0 @@
-const Room = require("../models/Rooms");
-const JoinRoom = (req, res) => {
-  console.log(req.body);
-  res.json({ status: 200, msg: "room joined" });
-};
-module.exports = JoinRoom;
